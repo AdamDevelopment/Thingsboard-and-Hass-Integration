10ec3cd3593962be71cf0d8f19a72077eb394103
Adam <77069838+AdamDevelopment@users.noreply.github.com>
2023-10-15 21:35:40 +0200

    Working Data send via MQTT to thingsboard - SHT35

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,62 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <SHT3x.h>
 
+// WiFi credentials
 const char* ssid = "Nie mam wi-fi";
 const char* password = "makaronzpomidorami";
-const char* thingsboardServer = "thingsboardrpi.duckdns.org";
-const char* accessToken = "95qx4nyy0no8vig0ib0a";
 
-WiFiClient client;
+// ThingsBoard MQTT broker settings
+const char* mqttServer = "thingsboardrpi.duckdns.org";
+const int mqttPort = 1883;
+const char* mqttToken = "og3ms9bts9x8a9agm00c";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+SHT3x Sensor_H(0x44);
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
-
+  Sensor_H.Begin();
   // Connect to Wi-Fi
-  Serial.println();
-  Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
   Serial.println("Connected to WiFi");
+
+  // Connect to MQTT broker
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP32",mqttToken, NULL)) {
+      Serial.println("Connected to MQTT");
+    } else {
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
-  // Replace these values with your sensor data
-  String data = "{ \"temperature\": 25.5, \"humidity\": 50.2 }"; // JSON format
-
-  // Create an HTTP request
-  String url = "/api/v1/" + String(accessToken) + "/telemetry"; // ThingsBoard telemetry endpoint
-
-  // Send the HTTP POST request to ThingsBoard
-  if (client.connect(thingsboardServer, 80)) {
-    Serial.println("Connected to ThingsBoard");
-    client.print("POST " + url + " HTTP/1.1\r\n");
-    client.print("Host: " + String(thingsboardServer) + "\r\n");
-    client.print("Content-Type: application/json\r\n");
-    client.print("Content-Length: " + String(data.length()) + "\r\n");
-    client.print("\r\n");
-    client.print(data);
-    client.stop();
-    Serial.println("Data sent to ThingsBoard");
-  } else {
-    Serial.println("Unable to connect to ThingsBoard");
+  if (!client.connected()) {
+    while (!client.connected()) {
+      if (client.connect("ESP32",mqttToken, NULL)) {
+        Serial.println("Reconnected to MQTT");
+      } else {
+        delay(5000);
+      }
+    }
   }
+  Sensor_H.UpdateData();
+  float temp = Sensor_H.GetTemperature();
+  float hum = Sensor_H.GetRelHumidity();
 
-  // Wait for a fixed interval before sending the next update
-  delay(15000); // Send data every 15 seconds
+  char payload[100];
+  snprintf(payload, sizeof(payload), "{\"temperature\": %.2f, \"humidity\": %.2f}", temp, hum);
+  client.publish("v1/devices/me/telemetry", payload);
+  Serial.println(temp);
+  Serial.println(hum);
+  delay(5000); // Send data every 5 seconds
 }
